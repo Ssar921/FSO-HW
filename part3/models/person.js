8db9562e325bb83b83f5469aa3497973d8d1262f
Ssar921 <51752696+Ssar921@users.noreply.github.com>
2026-08-05 22:17:25 +0500

    Dist removed, persons.js console statement removed

--- part3/models/person.js
+++ part3/models/person.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
 
-console.log("connecting to", url);
+// console.log("connecting to", url);
 mongoose.set("strictQuery", false);
 mongoose
 	.connect(url, { family: 4 })
